44a7f06a977f4fb63e55aa2d63efbe8dda4f08b7
OmarFarooqKhan <49125540+OmarFarooqKhan@users.noreply.github.com>
2020-07-07 14:54:24 +0100

    Add files via upload

---
+++ messaging-system/ClientReceiver.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.net.*;
+
+// Gets messages from other clients via the server (by the
+// ServerSender thread).
+
+public class ClientReceiver extends Thread {
+
+  private BufferedReader server;
+
+  ClientReceiver(BufferedReader server) {
+    this.server = server;
+  }
+ 
+
+  public void run() {
+    // Print to the user whatever we get from the server:
+    try {
+      while (true) {
+        String s = server.readLine(); // Matches FFFFF in ServerSender.java
+        
+        if (s != null)                
+          System.out.println(s);
+        else if(s.equals("Quit")) {
+    			System.out.println("Quitting");	
+        		break;
+        }
+        else
+          Report.errorAndGiveUp("Server seems to have died"); 
+      }
+      server.close();
+    }
+    catch (IOException e) {
+      Report.errorAndGiveUp("Server seems to have died " + e.getMessage());
+    }
+  }
+}
+
+/*
+
+ * The method readLine returns null at the end of the stream
+
+ * It may throw IoException if an I/O error occurs
+
+ * See https://docs.oracle.com/javase/8/docs/api/java/io/BufferedReader.html#readLine--
+
+
+ */
